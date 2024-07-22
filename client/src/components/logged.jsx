b05464e259cdc9957ad7e2b0fb09855f54ff8ab9
spaghetti-mafia1 <155222754+spaghetti-mafia1@users.noreply.github.com>
2024-07-22 15:15:47 +0300

    home

--- client/src/components/logged.jsx
+++ client/src/components/logged.jsx
@@ -1,26 +1,26 @@
-import { Modal } from 'flowbite-react';
-import { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
-import Sidebar from './sidebarHome'; // Import the Sidebar component
-import PostCard from './PostCard'; // Import the new PostCard component
+import Sidebar from './sidebarHome';
+import PostCard from './PostCard';
 import { HiMenu } from 'react-icons/hi';
-import InfiniteScroll from 'react-infinite-scroll-component';
+import { Button } from 'flowbite-react';
 
 export default function AllPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [posts, setPosts] = useState([]);
-  const [hasMore, setHasMore] = useState(true);
-  const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [showMore, setShowMore] = useState(false);
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [startIndex, setStartIndex] = useState(0);
-  const fetchedPostIds = useRef(new Set());
   const limit = 10;
+  const fetchedPostIds = useRef(new Set());
 
   useEffect(() => {
     fetchPosts(0); // Fetch initial posts
   }, []); // Empty dependency array ensures it runs once when the component mounts
 
   const fetchPosts = async (index) => {
+    setLoading(true);
     try {
       const res = await fetch(`/api/files/getallposts?startIndex=${index}&limit=${limit}`);
       const data = await res.json();
@@ -28,19 +28,22 @@ export default function AllPosts() {
         const newPosts = data.posts.filter(post => !fetchedPostIds.current.has(post._id));
         newPosts.forEach(post => fetchedPostIds.current.add(post._id));
         setPosts((prevPosts) => [...prevPosts, ...newPosts]);
-        setHasMore(newPosts.length >= limit);
+        setShowMore(newPosts.length >= limit);
         setStartIndex(index + limit);
       } else {
-        console.log(data.message);
+        console.error(data.message);
       }
     } catch (error) {
-      console.log(error.message);
+      console.error(error.message);
     }
+    setLoading(false);
   };
 
   const fetchMorePosts = () => {
     fetchPosts(startIndex);
   };
+
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
 
   // Group posts by category
   const groupedPosts = posts.reduce((acc, post) => {
@@ -49,51 +52,45 @@ export default function AllPosts() {
     return acc;
   }, {});
 
-  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
-
   return (
     <div className="flex">
       <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
 
-      {/* Main Content */}
       <div className={`flex-1 mt-4 transition-all duration-300 ${sidebarOpen ? 'ml-0 md:ml-80' : 'ml-5 md:ml-80'}`}>
         <div className="p-4">
           <button className="md:hidden mb-4" onClick={toggleSidebar}>
             <HiMenu className="h-6 w-6 text-gray-700" />
           </button>
           <div className="p-1">
-            <InfiniteScroll
-              dataLength={posts.length}
-              next={fetchMorePosts}
-              hasMore={hasMore}
-              loader={<p className="text-center">Loading...</p>}
-              endMessage={<p className="text-center">No more posts available!</p>}
-            >
-              {Object.keys(groupedPosts).length > 0 ? (
-                <>
-                  {Object.keys(groupedPosts).map((category) => (
-                    <div key={category} className="mb-6">
-                      <h2 className="text-xl font-bold uppercase mb-4 mt-16">{category}</h2>
-                      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-                        {groupedPosts[category].map((post) => (
-                          <PostCard key={post._id} post={post} /> // Use PostCard component
-                        ))}
+            {loading ? (
+              <div className="text-center text-orange-600">Loading...</div>
+            ) : (
+              <div>
+                {Object.keys(groupedPosts).length > 0 ? (
+                  <>
+                    {Object.keys(groupedPosts).map((category) => (
+                      <div key={category} className="mb-6">
+                        <h2 className="text-xl font-bold uppercase mb-4 mt-16">{category}</h2>
+                        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+                          {groupedPosts[category].map((post) => (
+                            <PostCard key={post._id} post={post} />
+                          ))}
+                        </div>
                       </div>
-                    </div>
-                  ))}
-                </>
-              ) : (
-                <p className="text-center">No posts available!</p>
-              )}
-            </InfiniteScroll>
-            <Modal
-              show={showModal}
-              onClose={() => setShowModal(false)}
-              popup
-              size="md"
-            >
-              <Modal.Header />
-            </Modal>
+                    ))}
+                  </>
+                ) : (
+                  <p className="text-center">No posts available!</p>
+                )}
+                {showMore && (
+                  <div className="text-center mt-4">
+                    <Button className="bg-blue-500 hover:bg-blue-600 text-white" onClick={fetchMorePosts}>
+                      Show More
+                    </Button>
+                  </div>
+                )}
+              </div>
+            )}
           </div>
         </div>
       </div>
