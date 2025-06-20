2162ee238c0500a6d6cee90bea2c7bf679cac02f
Tedd Chege <155222754+Tedd-Chege@users.noreply.github.com>
2025-06-20 22:25:53 +0300

    Update OnlyAdminPrivateRoute.jsx
    not

--- client/src/components/OnlyAdminPrivateRoute.jsx
+++ client/src/components/OnlyAdminPrivateRoute.jsx
@@ -4,7 +4,7 @@ import { Outlet, Navigate } from 'react-router-dom';
 export default function OnlyAdminPrivateRoute() {
   const { currentUser } = useSelector((state) => state.user);
 
-  return currentUser && (currentUser.isSuperAdmin || currentUser.isOverallAdmin || currentUser.isNormalAdmin) ? (
+  return currentUser && (currentUser.isAdmin || currentUser.isOverallAdmin || currentUser.isNormalAdmin) ? (
     <Outlet />
   ) : (
     <Navigate to='/sign-in' />
