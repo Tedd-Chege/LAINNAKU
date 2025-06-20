a7300a3c43c2f9e08a0dadb108a6b5de58466b51
Tedd Chege <155222754+Tedd-Chege@users.noreply.github.com>
2025-06-20 21:07:11 +0300

    Update Home.jsx
    super

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
   const { currentUser } = useSelector((state) => state.user);
 
   // Check if currentUser is defined and isAdmin property is true
-  const isAdmin = currentUser?.isAdmin || currentUser?.isOverallAdmin || currentUser?.isNormalAdmin;
+  const isSuperAdmin = currentUser?.isAdmin || currentUser?.isOverallAdmin || currentUser?.isNormalAdmin;
 
   return (
     <div className="">
